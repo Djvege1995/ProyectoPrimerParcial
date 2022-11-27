7d51328d32ab0c15cfc556befd3634eda5515f79
dvenegas <119146281+Djvege1995@users.noreply.github.com>
2022-11-27 09:34:09 +0000

    Funciones Get

--- colaborador.java
+++ colaborador.java
@@ -7,5 +7,28 @@ class colaborador extends personas{
     this.tipo=tipo;
     
   }
-  
+  public String getPuesto(){
+    return puesto;
+  }
+  public String getCedula(){
+    return ci;
+  }
+  public String getNombre(){
+    return nombre;
+  }
+  public String getPuesto(){
+    return puesto;
+  }
+  public String getMail(){
+    return mail;
+  }
+  public String getTelefono(){
+    return telefono;
+  }
+  public tipoEmpleado getTipo(){
+    return tipoEmpleado;
+  }
+  public void mostrarInfo(){
+    System.out.printl("Nombre:"+nombre+"\nCédula:"+c)
+  }
 }
